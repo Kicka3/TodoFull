23bce2637779b13c08787b645eacf462e58cfa5c
Kirill <kirill_ozornin@mail.ru>
2023-09-26 19:53:31 +0200

    create html

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,22 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
-import TodoListCard, {TasksType} from "./components/todoListCard/TodoListCard";
+import TodoListCard from "./components/todoListCard/TodoListCard";
 
-
-export type FilteredValueType = 'all' | 'active' | 'completed';
 
 function App() {
-    const todoListTitle1: string = "What to learn";
 
-    const [tasks, setTasks] = useState<Array<TasksType>>([
-        {id: crypto.randomUUID(), titleTask: "HTML&CSS", isDone: true},
-        {id: crypto.randomUUID(), titleTask: "JS", isDone: true},
-        {id: crypto.randomUUID(), titleTask: "TS", isDone: false},
-        {id: crypto.randomUUID(), titleTask: "React", isDone: false},
-        {id: crypto.randomUUID(), titleTask: "Redux", isDone: false},
-    ]);
+    const tasks = [
+        {id: 1, titleTask: "HTML&CSS", isDone: true},
+        {id: 2, titleTask: "JS", isDone: true},
+        {id: 3, titleTask: "TS", isDone: false},
+        {id: 4, titleTask: "React", isDone: false},
+        {id: 5, titleTask: "Redux", isDone: false},
+    ];
 
-    const addTask = (titleTask: string) => {
-        const newTask: TasksType = {
-            id: crypto.randomUUID(),
-            titleTask,
-            isDone: false,
-        }
-        const nexState: Array<TasksType> = [newTask, ...tasks]
-        setTasks(nexState)
-    };
 
-    const removeTask = (taskId: string) => {
-
-        setTasks(tasks.filter(t => t.id !== taskId));
-    }
-
-    const [filter, setFilter] = useState<FilteredValueType>('all')
-
-    const getFilteredTaskForRender = (allTasks: Array<TasksType>, filterValue: FilteredValueType) => {
-        if (filterValue === 'active') {
-            return allTasks.filter(task => !task.isDone)
-        }
-        if (filterValue === 'completed') {
-            return allTasks.filter(task => !task.isDone)
-        }
-        return allTasks;
-    }
-
-    const filteredTasksForRender: Array<TasksType> = getFilteredTaskForRender(tasks, filter)
-    const changeFilter = (nextFilterValue: FilteredValueType) => {
-        setFilter(nextFilterValue);
-    }
     return (
         <div className="App">
-            <TodoListCard title={todoListTitle1}
-                          tasks={filteredTasksForRender}
-                          removeTask={removeTask}
-                          changeFilter={changeFilter}
-                          addTask={addTask}/>
+            <TodoListCard/>
         </div>
     )
 }
--- src/components/todoListCard/TodoListCard.tsx
+++ src/components/todoListCard/TodoListCard.tsx
@@ -1,106 +1,33 @@
-import React, {FC, useRef, useState, KeyboardEvent} from 'react';
-import {FilteredValueType} from "../../App";
+import React from 'react';
 
 
-export type TasksType = {
-    id: string,
-    titleTask: string,
-    isDone: boolean,
-}
-
-type TodoListCardPropsType = {
-    title: string,
-    tasks: Array<TasksType>,
-    removeTask: (taskId: string) => void,
-    changeFilter: (nextFilterValue: FilteredValueType) => void,
-    addTask: (titleTask: string) => void,
-}
-
-const TodoListCard: FC<TodoListCardPropsType> = ({
-                                                     title, tasks,
-                                                     removeTask,
-                                                     changeFilter,
-                                                     addTask,
-                                                 }) => {
-    const listItems: Array<JSX.Element> | JSX.Element = tasks.map(el => {
-
-        const onClickRemoveTaskHandler = () => {
-            console.log(el.id)
-            removeTask(el.id)
-        }
-
-        return (
-            <li className={"TodoListWrapperLi"}
-                key={el.id}>
-                <input className={"TodoTask"}
-                       type="checkbox"
-                       checked={el.isDone}
-                />
-
-                <span>{el.titleTask}</span>
-
-                <button className={"RemoveBtn"}
-                        onClick={onClickRemoveTaskHandler}>✖️
-                </button>
-            </li>
-        )
-    });
-
-    const TasksList: JSX.Element = tasks.length
-        ? <ul>{listItems}</ul>
-        : <span className={"EmptyTasksList"}>Your tasks lists is empty</span>
-
-    const [newTaskTitle, setNewTaskTitle] = useState('');
-
-    const onClickAddTask = () => {
-        addTask(newTaskTitle)
-        setNewTaskTitle('')
-    }
-
-    const onKeyDownAddTask = (event: KeyboardEvent<HTMLInputElement>) => event.key === 'Enter' && onClickAddTask();
-
-    const isAddBtnDisabled = !newTaskTitle || title.length >= 15
-
-    const OnClickAllHandler = () => changeFilter('all');
-    const OnClickActiveHandler = () => changeFilter('active');
-    const OnClickCompletedHandler = () => changeFilter('completed');
+const TodoListCard = () => {
 
     return (
         <div className={"CardWrapper"}>
-            <h3 className={"CardTitle"}>{title}</h3>
+            <h3 className={"CardTitle"}></h3>
             <div>
                 <div className={"TodoForm"}>
                     <input
                         className={"TodoInput"}
-                        value={newTaskTitle}
-                        onChange={(e) => setNewTaskTitle(e.target.value)}
                         placeholder={"What is the task today?"}
-                        maxLength={16}
-                        onKeyDown={onKeyDownAddTask}
                     />
 
-                    <button className={"TodoAddBtn"}
-                            disabled={isAddBtnDisabled}
-                            onClick={onClickAddTask}>+
-                    </button>
-                    <div>
-                        <span className={"EmptyLine"}>{newTaskTitle.length < 15
-                            ? 'Enter a new task title...'
-                            : 'Your title is too long'}</span>
-                    </div>
+                    <button className={"TodoAddBtn"}>+</button>
+
                 </div>
 
                 <div className={"TodoListsWrapper"}>
                     <ul>
-                        {TasksList}
+
                     </ul>
                 </div>
             </div>
 
             <div className={"FilterBtnWrapper"}>
-                <button className={"FilterBtn"} onClick={OnClickAllHandler}>All</button>
-                <button className={"FilterBtn"} onClick={OnClickActiveHandler}>Active</button>
-                <button className={"FilterBtn"} onClick={OnClickCompletedHandler}>Completed</button>
+                <button className={"FilterBtn"}>All</button>
+                <button className={"FilterBtn"}>Active</button>
+                <button className={"FilterBtn"}>Completed</button>
             </div>
 
         </div>
